e738751530730a4af732d71585650d92e38069e6
Jason <sanchezjason@me.com>
2024-04-10 21:51:46 -0600

    test_driven_development/swiftui/tip_calculator- whenCalculatingTipBasedOnNegativeTotalAmount XCTestCase

--- TipCalculatorTDD/TipCalculatorUnitTest/TipCalculatorUnitTest.swift
+++ TipCalculatorTDD/TipCalculatorUnitTest/TipCalculatorUnitTest.swift
@@ -10,9 +10,19 @@ import XCTest
 class whenCalculatingTipBasedOnTotalAmount: XCTestCase {
 
     func testShouldCalculateTipSuccessfully() {
-        
         let tipCalculator = TipCalculator()
         let tip = tipCalculator.calculate(total: 100, tipPercentage: 0.1)
         XCTAssertEqual(10, tip)
     }
+}
+
+class whenCalculatingTipBasedOnNegativeTotalAmount: XCTestCase {
+    
+    func testShouldThrowInvalidInputException() {
+        let tipCalculator = TipCalculator()
+        XCTAssertThrowsError(try tipCalculator.calculate(total: -100, tipPercentage: 0.1), "") { error in
+            XCTAssertEqual(error as! TipCalculatorError, TipCalculatorError.invalidInput)
+        }
+    }
+    
 }
