cca198581a68e6bc485b54551854fb77ecf4959d
Jason <sanchezjason@me.com>
2024-04-10 21:55:47 -0600

    test_driven_development/swiftui/tip_calculator- TipCalculatorError

--- TipCalculatorTDD/TipCalculatorTDD/Models/TipCalculator.swift
+++ TipCalculatorTDD/TipCalculatorTDD/Models/TipCalculator.swift
@@ -7,8 +7,16 @@
 
 import Foundation
 
+enum TipCalculatorError: Error {
+    case invalidInput
+}
+
 class TipCalculator {
-    func calculate(total: Double, tipPercentage: Double) -> Double {
+    func calculate(total: Double, tipPercentage: Double) throws -> Double {
+        
+        if total < 0 {
+            throw TipCalculatorError.invalidInput
+        }
         return total * tipPercentage
     }
 }
