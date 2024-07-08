4ec4a214a0a29002499ef46fb28eed57e97e9ef7
Jason <sanchezjason@me.com>
2024-07-08 16:46:25 -0600

    test_driven_development/TipCalculator/UITests text_field_test, tip_percentage_test

--- TipCalculatorTDD/TipCalculatorUITests/TipCalculatorUITests.swift
+++ TipCalculatorTDD/TipCalculatorUITests/TipCalculatorUITests.swift
@@ -8,11 +8,16 @@
 import XCTest
 
 class when_content_view_is_shown: XCTestCase {
-
-    func test_should_make_sure_that_the_total_text_field_contains_default_value() {
-        let app = XCUIApplication()
+    
+    private var app: XCUIApplication!
+    
+    override func setUp() {
+        app = XCUIApplication()
         continueAfterFailure = false
         app.launch()
+    }
+
+    func test_should_make_sure_that_the_total_text_field_contains_default_value() {
         
         let totalTextField = app.textFields["totalTextField"]
         
@@ -20,14 +25,15 @@ class when_content_view_is_shown: XCTestCase {
     }
     
     func test_should_make_sure_the_20_percent_default_tip_option_is_selected() {
-        let app = XCUIApplication()
-        continueAfterFailure = false
-        app.launch()
         
         let tipPercenatgeSegmentedControl = app.segmentedControls["tipPercenatgeSegmentedControl"]
         let segmentedControlButton = tipPercenatgeSegmentedControl.buttons.element(boundBy: 1)
         
         XCTAssertEqual(segmentedControlButton.label, "20%")
         XCTAssertTrue(segmentedControlButton.isSelected)
+    }
+    
+    override func tearDown() {
+        // clean up
     }
 }
