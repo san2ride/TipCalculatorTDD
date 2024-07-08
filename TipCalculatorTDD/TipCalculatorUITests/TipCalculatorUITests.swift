21891a7bf9ad79741c907ffa807716898a9766f2
Jason <sanchezjason@me.com>
2024-07-08 16:03:24 -0600

    test_driven_development/TipCalculator/UITests setup

---
+++ TipCalculatorTDD/TipCalculatorUITests/TipCalculatorUITests.swift
@@ -0,0 +1,15 @@
+//
+//  TipCalculatorUITests.swift
+//  TipCalculatorUITests
+//
+//  Created by Jason Sanchez on 7/8/24.
+//
+
+import XCTest
+
+class when_content_view_is_shown: XCTestCase {
+
+    func test_should_make_sure_that_the_total_text_field_contains_default_value() {
+        
+    }
+}
